4e6b49585ad2e8bc6a9f0e94c0260e2a3643484b
jeevithan003 <jeevijeevithan24@gmail.com>
2024-05-03 22:53:37 +0530

    userinput in py

---
+++ userinput.py
@@ -0,0 +1,4 @@
+player=input("enter the football player")
+goal_ratio=float(input("enter the player goal ratio"))
+
+print(f"the football palyer  {player} has a {goal_ratio} is completely insane in football")
