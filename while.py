6e9c65ee1fa1ddcf633325ca2c12c5fb6ea9111d
jeevithan003 <jeevijeevithan24@gmail.com>
2024-05-04 21:00:49 +0530

    while loop in py

---
+++ while.py
@@ -0,0 +1,6 @@
+respone=input("enter the football palyer you alike the most.. also g to giut")
+while respone!="g":
+    print(f"your fav player is {respone}")
+    respone=input("enter the football palyer you alike the most.. also g to giut")
+
+print("bye bro")
