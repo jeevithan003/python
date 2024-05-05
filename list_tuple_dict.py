321b752bdf7941c6ab41bdd3069d6e52d6e6043a
jeevithan003 <jeevijeevithan24@gmail.com>
2024-05-05 17:32:13 +0530

    collection

---
+++ list_tuple_dict.py
@@ -0,0 +1,30 @@
+#list
+
+mylist=[1,2,3,4,5,6,7,4,4]
+print(dir([]))
+print(mylist.append(25))    
+print(mylist.count(4))
+print(mylist.index(6))
+print(mylist.insert(1,"mbappe"))
+print(mylist.pop(2))
+print(mylist.remove(6))
+print(mylist.reverse())
+mylist = [1, 2, 3, 4, 5, 6, 7, 4, 4]
+
+print( sorted(mylist))
+
+# tuple
+mytuple=(1,2,3,4,5,6)
+print(dir(()))
+print(mytuple.count(1))
+print(mytuple.index(3))
+
+#dict
+mydict={1,2,3,4,5,6,7}
+
+print(dir(mydict))
+print(mydict.add(10))
+print(mydict.remove(2))
+print(dir({}))
+print(mydict)
+
