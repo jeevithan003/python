a8474a18e246bde86308e810993c5fec09ad06d2
jeevithan003 <jeevijeevithan24@gmail.com>
2024-05-14 16:55:43 +0530

    pattern

--- opps.py
+++ opps.py
@@ -1,19 +1,12 @@
-class Madrid:
-    def __init__(self,player,number) -> None:
-       self.player=player
-       self.number=number
-    def details(self):
-        print(f"the player name is  {self.player}")
-        print(f"the jercy number  is  {self.number}")
-    def ucl(self):
-        print("valid to play ucl..")
-    def pl(self):
-        print("not enough to enter ucl..")
-
-player1=Madrid("Kylian_mbappe",7)
-player2=Madrid("Haaland",9)
-
-player1.details()
-player1.ucl()
-player2.details()
-player2.pl()
+class m():
+    def __init__(self) -> None:
+        print("A")
+class n(m):
+    def __init__(self) -> None:
+        super().__init__()
+        print("B")
+class o(n,m):
+    def __init__(self) -> None:
+        super().__init__()
+        print("C")
+onb=o()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-print(help("modules"))
+@staticmethod
